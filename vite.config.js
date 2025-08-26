75f8a4adcab4e02a2334930a47a87e95044ebdc7
zerohcool <79310263+zerohcool@users.noreply.github.com>
2025-08-26 16:32:15 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 agrega base con el nombre EXACTO de tu repo de GitHub
 export default defineConfig({
   plugins: [react()],
+  base: '/Informe-MMPP-Enaex/'   // 👈 cambia por el nombre de tu repo
 })
